47aa7a4663df6918b8369869253f8b972e94de26
Renzo Vizarreta <rvizarreta14@gmail.com>
2026-02-27 14:59:22 -0600

    back to my signal

--- src/MCStudies/GenericFlux_Tester.cxx
+++ src/MCStudies/GenericFlux_Tester.cxx
@@ -279,7 +279,7 @@ void GenericFlux_Tester::AddICARUS1muNp0piVariablesToTree() {
 void GenericFlux_Tester::FillICARUS1muNp0piVariablesToTree(FitEvent *event) {
 
   unsigned int nMu_1muNp0pi(0), nP_1muNp0pi(0), nPi_1muNp0pi(0);
-  unsigned int nPhoton_1muNp0pi(0), nMesons_1muNp0pi(0), nBaryonsAndPi0_1muNp0pi(0);
+  unsigned int nPhoton_1muNp0pi(0), nElectron_1muNp0pi(0), nMesons_1muNp0pi(0), nBaryonsAndPi0_1muNp0pi(0);
   double maxMomentumP_1muNp0pi = -999.;
   bool passProtonPCut_1muNp0pi = false;
 
@@ -300,7 +300,6 @@ void GenericFlux_Tester::FillICARUS1muNp0piVariablesToTree(FitEvent *event) {
 
     // ICARUS 1muNp0pi
 
-
     // All FS protons for generic purpose
     if(pdgc==2212){
       protons.push_back(event->PartInfo(i));
@@ -313,23 +312,28 @@ void GenericFlux_Tester::FillICARUS1muNp0piVariablesToTree(FitEvent *event) {
       if (PassMuonPCut) nMu_1muNp0pi+=1;
     }
 
-    if ( abs(pdgc) == 2212 ) {
+      if ( abs(pdgc) == 2212 ) {
       nP_1muNp0pi+=1;
       if ( momentum > maxMomentumP_1muNp0pi ) {
         maxMomentumP_1muNp0pi = momentum;
-        passProtonPCut_1muNp0pi = (momentum > 0.4 && momentum < 1.);
+        passProtonPCut_1muNp0pi = (momentum > 0.31);
       }
     }
 
-    if ( abs(pdgc) == 111 || abs(pdgc) == 211 ) nPi_1muNp0pi+=1;
-    // CHECK A SIMILAR DEFINITION AS MINERVA FOR EXTRA REJECTION OF UNWANTED THINGS IN SIGNAL DEFN.
-    if ( abs(pdgc) == 22 && part_4mom.E()/1000. > 0.01 ) nPhoton_1muNp0pi+=1;
-    else if ( abs(pdgc) == 211 || abs(pdgc) == 321 || abs(pdgc) == 323 ||
-              pdgc == 111 || pdgc == 130 || pdgc == 310 || pdgc == 311 ||
-              pdgc == 313 || abs(pdgc) == 221 || abs(pdgc) == 331 ) nMesons_1muNp0pi+=1;
-    else if ( pdgc == 3112 || pdgc == 3122 || pdgc == 3212 || pdgc == 3222 ||
-              pdgc == 4112 || pdgc == 4122 || pdgc == 4212 || pdgc == 4222 ||
-              pdgc == 411 || pdgc == 421 || pdgc == 111 ) nBaryonsAndPi0_1muNp0pi+=1;
+    // Pion veto with momentum threshold
+    if ( (abs(pdgc) == 211) && momentum > 0.087 ) nPi_1muNp0pi+=1;
+    if ( abs(pdgc) == 111 ) nPi_1muNp0pi+=1;
+    // Photon veto with momentum threshold
+    if ( abs(pdgc) == 22 && part_4mom.E()/1000. > 0.025 ) nPhoton_1muNp0pi+=1;
+    // Electron veto with momentum threshold
+    if ( abs(pdgc) == 11 && momentum > 0.0255 ) nElectron_1muNp0pi+=1;
+//     else if ( abs(pdgc) == 321 || abs(pdgc) == 323 ||
+//               pdgc == 111 || pdgc == 130 || pdgc == 310 || pdgc == 311 ||
+//               pdgc == 313 || abs(pdgc) == 221 || abs(pdgc) == 331 ) nMesons_1muNp0pi+=1;
+//     else if ( pdgc == 3112 || pdgc == 3122 || pdgc == 3212 || pdgc == 3222 ||
+//               pdgc == 4112 || pdgc == 4122 || pdgc == 4212 || pdgc == 4222 ||
+//               pdgc == 411 || pdgc == 421 || pdgc == 111 ) nBaryonsAndPi0_1muNp0pi+=1;
+
 
   }
 
@@ -337,8 +341,7 @@ void GenericFlux_Tester::FillICARUS1muNp0piVariablesToTree(FitEvent *event) {
                              nP_1muNp0pi>0 && passProtonPCut_1muNp0pi &&
                              nPi_1muNp0pi==0 &&
                              nPhoton_1muNp0pi==0 &&
-                             nMesons_1muNp0pi==0 &&
-                             nBaryonsAndPi0_1muNp0pi==0;
+                             nElectron_1muNp0pi==0;
 
   bool IsAntiNu = event->GetNeutrinoIn()->fPID<0;
 
