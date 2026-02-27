9683ea0632fa7ef6e5c0c6bc1dc461b6bfe1c060
Renzo Vizarreta <rvizarreta14@gmail.com>
2026-02-27 15:12:40 -0600

    remove pi0s

--- src/MCStudies/GenericFlux_Tester.cxx
+++ src/MCStudies/GenericFlux_Tester.cxx
@@ -322,7 +322,6 @@ void GenericFlux_Tester::FillICARUS1muNp0piVariablesToTree(FitEvent *event) {
 
     // Pion veto with momentum threshold
     if ( (abs(pdgc) == 211) && momentum > 0.087 ) nPi_1muNp0pi+=1;
-    if ( abs(pdgc) == 111 ) nPi_1muNp0pi+=1;
     // Photon veto with momentum threshold
     if ( abs(pdgc) == 22 && part_4mom.E()/1000. > 0.025 ) nPhoton_1muNp0pi+=1;
     // Electron veto with momentum threshold
