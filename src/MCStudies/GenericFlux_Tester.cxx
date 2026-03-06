6cbf8c64b0abfea1ef1e6e1aefe550c3425c8fdc
Renzo Vizarreta <rvizarreta14@gmail.com>
2026-03-05 20:51:34 -0600

    back to the veto of particles

--- src/MCStudies/GenericFlux_Tester.cxx
+++ src/MCStudies/GenericFlux_Tester.cxx
@@ -327,12 +327,12 @@ void GenericFlux_Tester::FillICARUS1muNp0piVariablesToTree(FitEvent *event) {
     // if ( abs(pdgc) == 11 && momentum > 0.0255 ) nElectron_1muNp0pi+=1;
     // Photon veto with momentum threshold
     if ( abs(pdgc) == 22 && part_4mom.E()/1000. > 0.025 ) nPhoton_1muNp0pi+=1;
-//     else if ( abs(pdgc) == 211 || abs(pdgc) == 321 || abs(pdgc) == 323 ||
-//               pdgc == 111 || pdgc == 130 || pdgc == 310 || pdgc == 311 ||
-//               pdgc == 313 || abs(pdgc) == 221 || abs(pdgc) == 331 ) nMesons_1muNp0pi+=1;
-//     else if ( pdgc == 3112 || pdgc == 3122 || pdgc == 3212 || pdgc == 3222 ||
-//               pdgc == 4112 || pdgc == 4122 || pdgc == 4212 || pdgc == 4222 ||
-//               pdgc == 411 || pdgc == 421 || pdgc == 111 ) nBaryonsAndPi0_1muNp0pi+=1;
+    else if ( abs(pdgc) == 211 || abs(pdgc) == 321 || abs(pdgc) == 323 ||
+              pdgc == 111 || pdgc == 130 || pdgc == 310 || pdgc == 311 ||
+              pdgc == 313 || abs(pdgc) == 221 || abs(pdgc) == 331 ) nMesons_1muNp0pi+=1;
+    else if ( pdgc == 3112 || pdgc == 3122 || pdgc == 3212 || pdgc == 3222 ||
+              pdgc == 4112 || pdgc == 4122 || pdgc == 4212 || pdgc == 4222 ||
+              pdgc == 411 || pdgc == 421 || pdgc == 111 ) nBaryonsAndPi0_1muNp0pi+=1;
 
 
   }
