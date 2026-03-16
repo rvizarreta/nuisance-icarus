2f5a8fce1bf65b8599b1250aea0738a8b8edc0f1
Renzo Vizarreta <rvizarreta14@gmail.com>
2026-03-16 11:19:26 -0600

    removing electron veto and changing photon energy

--- src/MCStudies/GenericFlux_Tester.cxx
+++ src/MCStudies/GenericFlux_Tester.cxx
@@ -324,9 +324,9 @@ void GenericFlux_Tester::FillICARUS1muNp0piVariablesToTree(FitEvent *event) {
     if ( (abs(pdgc) == 211) && momentum > 0.087 ) nPi_1muNp0pi+=1;
     if ( abs(pdgc) == 111 ) nPi_1muNp0pi+=1;
     // Electron veto with momentum threshold
-    if ( abs(pdgc) == 11 && momentum > 0.0255 ) nElectron_1muNp0pi+=1;
+    //if ( abs(pdgc) == 11 && momentum > 0.0255 ) nElectron_1muNp0pi+=1;
     // Photon veto with momentum threshold
-    if ( abs(pdgc) == 22 && part_4mom.E()/1000. > 0.025 ) nPhoton_1muNp0pi+=1;
+    if ( abs(pdgc) == 22 && part_4mom.E()/1000. > 0.01 ) nPhoton_1muNp0pi+=1;
     else if ( abs(pdgc) == 211 || abs(pdgc) == 321 || abs(pdgc) == 323 ||
               pdgc == 111 || pdgc == 130 || pdgc == 310 || pdgc == 311 ||
               pdgc == 313 || abs(pdgc) == 221 || abs(pdgc) == 331 ) nMesons_1muNp0pi+=1;
